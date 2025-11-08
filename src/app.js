f732a39a694a30877991ec67aa2d6d1a0da733aa
sunilroat-code <sunilroat7takatak@gmail.com>
2025-11-08 19:44:11 +0530

    route changes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 const express=require("express");
 
 const app=express();
+  
+
+app.use("/op",(req,res)=>{
+       res.send("your op route ");
+});
+
+
+app.use("/feed/2",(req,res)=>{
+       res.send("your feed 2.0");
+});
+
 
 
 app.use("/feed",(req,res)=>{
-res.send("your feed ");
+     res.send("your feed ");
 });
+
+
+app.use("/",(req,res)=>{
+       res.send("your / route iss here ");
+});
+
 
 app.use((req,res)=>{
 res.send("hay this your server how are you");
