0456f857463d26fe996d19fc3e7c787012c26168
sunilroat-code <sunilroat7takatak@gmail.com>
2025-11-10 03:50:05 +0530

    error_handling

--- src/app.js
+++ src/app.js
@@ -1,59 +1,26 @@
 const express=require("express");
 
-
 const app=express();
 
-// const {auth}=require("./middleware/auth.js");
-
-function auth(req,res,next){
-    console.log("admin auth is getting checked");
-    const token="abc";
-    const check=token==="abc";
-    if(!check){
-        res.status(401).send("unauthorized access");
-    }else{
-        next();
-    }
-}
-  
-
-
-
-app.get("/admin/allprofile",auth,(req,res)=>{
-
-       res.send("your op route ");
+app.use("/",(err,req,res,next)=>{
+       if(err){
+              res.status(500).send("error happened");
+       }
+      
 });
 
-
-app.get("/admin/alldata",auth,(req,res)=>{
-       res.send("your op route ");
+app.get("/admin",(req,res)=>{
+      
+       throw new Error("jwenwio");
+       res.send("hi");
+       
 });
 
-
-app.get("/admin/check",auth,(req,res)=>{
-       res.send("your op route ");
+app.use("/",(err,req,res,next)=>{
+       if(err){
+              res.status(500).send("error happened");
+       }
 });
-
-app.get("/user",(req,res)=>{
-       res.send("your op route ");
-});
-
-
-app.use("/feed/2",(req,res)=>{
-       res.send("your feed 2.0");
-});
-
-
-
-app.get("/feed",(req,res)=>{
-     res.send("your feed ");
-});
-
-
-app.use("/c",(req,res)=>{
-       res.send("your / route iss here ");
-});
-
 
 
 
