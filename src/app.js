88cacb1edd1e0e9333ac9ff1fcd6128f439639dc
sunilroat-code <sunilroat7takatak@gmail.com>
2025-11-10 03:01:03 +0530

    middlewares are added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,40 @@
 const express=require("express");
 
+
 const app=express();
+
+// const {auth}=require("./middleware/auth.js");
+
+function auth(req,res,next){
+    console.log("admin auth is getting checked");
+    const token="abc";
+    const check=token==="abc";
+    if(!check){
+        res.status(401).send("unauthorized access");
+    }else{
+        next();
+    }
+}
   
 
-app.use("/op",(req,res)=>{
+
+
+app.get("/admin/allprofile",auth,(req,res)=>{
+
+       res.send("your op route ");
+});
+
+
+app.get("/admin/alldata",auth,(req,res)=>{
+       res.send("your op route ");
+});
+
+
+app.get("/admin/check",auth,(req,res)=>{
+       res.send("your op route ");
+});
+
+app.get("/user",(req,res)=>{
        res.send("your op route ");
 });
 
@@ -14,19 +45,16 @@ app.use("/feed/2",(req,res)=>{
 
 
 
-app.use("/feed",(req,res)=>{
+app.get("/feed",(req,res)=>{
      res.send("your feed ");
 });
 
 
-app.use("/",(req,res)=>{
+app.use("/c",(req,res)=>{
        res.send("your / route iss here ");
 });
 
 
-app.use((req,res)=>{
-res.send("hay this your server how are you");
-});
 
 
 app.listen(7777);
---
+++ src/middleware/auth.js
@@ -0,0 +1,14 @@
+// const auth=(req,res,next)=>{
+//     console.log("admin auth is getting checked");
+//     const token="abc";
+//     const check=token==="abhc";
+//     if(!check){
+//         res.status(401).send("unauthorized access");
+//     }else{
+//         next();
+//     }
+// }
+
+// module.exports={
+//     auth
+// };
