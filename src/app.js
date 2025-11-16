dd1a0f4bc753d1632570754b7761ad320d09e0a9
sunilroat-code <sunilroat7takatak@gmail.com>
2025-11-17 05:18:37 +0530

    all crud

--- src/app.js
+++ src/app.js
@@ -3,13 +3,19 @@ const connectDB=require("./config/database.js");
 const User=require("./models/user.js")
 const app=express();
 
+app.use(express.json());
+
 app.post("/signup",async(req,res)=>{
-       const user=new User({
-              firstName:"virat",
-              lastName:"kohli",
-              emailId:"virat@gmail.com",
-              password:"virat@1234",
-       });
+
+      console.log(req.body);
+      const user=new User(req.body);
+      
+       // const user=new User({
+       //        firstName:"virat",
+       //        lastName:"kohli",
+       //        emailId:"virat@gmail.com",
+       //        password:"virat@1234",
+       // });
      try{
        await user.save();
        // throw new exception();
@@ -20,6 +26,62 @@ app.post("/signup",async(req,res)=>{
      }
 
 });
+
+app.get("/user",async (req,res)=>{
+  const userEmail=req.body.emailId;
+  
+try{
+  console.log(userEmail);
+  const user =await User.findOne({emailId:userEmail});
+  if(!user){
+    res.status(404).send("user not found");
+  }else{
+    res.send(user);
+  }
+}
+catch(err){
+  res.status(400).send("Somethig went Wrong");
+}
+});
+
+app.get("/feed",async(req,res)=>{
+
+  try{
+  const user=await User.find({});
+  res.send(user);
+  }
+  catch(err){
+    res.status(400).send("Something went wrong ");
+
+  }
+})
+
+app.delete("/delete",async(req,res)=>{
+  const userId=req.body.userId;
+  try{
+    const user=await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+
+  }catch(err){
+    res.status(400).send("Something went wrong ");
+  }
+})
+
+app.patch("/patch",async(req,res)=>{
+  const userId=req.body.userId;
+  const data=req.body;
+  // console.log(data);
+
+  try{
+  const user=  await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"after"});
+    res.send("User updated succesfully");
+    console.log(user);
+  }
+  catch(err){
+    res.status(400).send("Something went wrong");
+  }
+
+})
 
 
 connectDB()
